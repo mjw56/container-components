0342d39423e89a3c6bfda353306b8a2e2baba7d6
Mike Wilcox <mwilcox56@gmail.com>
2015-06-15 21:21:21 -0400

    add utility for getting magnet uri

---
+++ src/example_one/utils/query.js
@@ -0,0 +1,22 @@
+var trackers = [
+  'udp://open.demonii.com:1337',
+  'udp://tracker.istole.it:80',
+  'http://tracker.yify-torrents.com/announce',
+  'udp://tracker.publicbt.com:80',
+  'udp://tracker.openbittorrent.com:80',
+  'udp://tracker.coppersurfer.tk:6969',
+  'udp://exodus.desync.com:6969',
+  'http://exodus.desync.com:6969/announce'
+];
+
+var API = {
+  upcoming: 'https://yts.to/api/upcoming.json', // https://yts.re/api#upcomingDocs
+  list: 'https://yts.to/api/v2/list_movies.json' // http://yts.to/api#list_movies
+};
+
+export function magnetURI(hash, title) {
+  var template = 'magnet:?xt=urn:btih:{HASH}&dn={TITLE}&tr=' + trackers.join('&tr=');
+  return template
+    .replace('{HASH}', hash)
+    .replace('{TITLE}', encodeURIComponent(title));
+}
