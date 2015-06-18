2fd72d600dcfbfd3c0a2e94bfdf5618da01af40f
Mike Wilcox <mwilcox56@gmail.com>
2015-06-17 20:09:24 -0400

    add an express server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var express = require('express');
+
+// TODO: Use socket.io
+
+const api = express()
+  .get('/torrent-stream', function(req, res) {
+    res.send({ data: 'torrent data' })
+  });
+
+const app = express()
+  .all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    next()
+  });
+
+app.use('/api', api)
+  .listen(3001)
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
